a3fe75f552088a944c659d7206044be34dadeea1
rostom17 <rostom825@gmail.com>
2025-02-03 02:01:54 +0600

    addition functionality added

--- lib/controller/operations.dart
+++ lib/controller/operations.dart
@@ -3,7 +3,7 @@ class Operations {
       String firstValue, String lastValue, String operator) {
     double x = double.tryParse(firstValue) ?? 0;
     double y = double.tryParse(lastValue) ?? 0;
-    print("$x, $y, $operator");
+
     switch (operator) {
       case "+":
         return x + y;
